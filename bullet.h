52e72e8c6aec3328fb666862b8922801885458b2
Meeeri08 <61042895+Meeeri08@users.noreply.github.com>
2020-03-10 13:52:17 +0100

    Base

---
+++ bullet.h
@@ -0,0 +1,24 @@
+#include "Entity.h"
+
+enum class DIRECTIONS
+{
+	UP, DOWN
+};
+
+class Bullet
+{
+private:
+
+	//Floats
+	float  speed;
+
+	//Bools
+	bool IsAlive;
+
+	//Directions
+	DIRECTIONS mDirection;
+
+public:
+
+	
+};
